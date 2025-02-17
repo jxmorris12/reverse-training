631f324c8bae2f365d0fe98327f1f971f7688cab
Jack Morris <jxmorris12@gmail.com>
2025-02-17 18:26:46 -0500

    try new loss

--- reverse_dense/distill.py
+++ reverse_dense/distill.py
@@ -45,6 +45,7 @@ def main(args):
         sync_tensorboard=False,
         project="dataset-distillation",
         config=args,
+        # mode="disabled",
     )
 
     args = type('', (), {})()
@@ -55,8 +56,6 @@ def main(args):
         args.batch_syn = args.dataset_size # Full-batch GD
 
     args.distributed = torch.cuda.device_count() > 1
-
-
     print('Hyper-parameters: \n', args.__dict__)
 
     student_net = transformers.AutoModelForCausalLM.from_pretrained(
@@ -92,15 +91,15 @@ def main(args):
     print('initialize synthetic data from random noise')
 
 
-    """ training """
+    #   training
     token_embeddings_syn = token_embeddings_syn.detach().to(args.device).requires_grad_(True)
     syn_lr = syn_lr.detach().to(args.device).requires_grad_(True)
+    # optimizer_token_embeddings = torch.optim.AdamW([token_embeddings_syn], lr=args.lr_img)
     optimizer_token_embeddings = torch.optim.SGD([token_embeddings_syn], lr=args.lr_img, momentum=0.5)
-    optimizer_lr = torch.optim.SGD([syn_lr], lr=args.lr_lr, momentum=0.5)
+    optimizer_lr = torch.optim.SGD([syn_lr], lr=args.lr_lr, momentum=0.5) # momentum=0.5
     optimizer_token_embeddings.zero_grad()
 
-    criterion = nn.CrossEntropyLoss().to(args.device)
-    print('%s training begins'%get_time())
+    print('%s [time] training begins'%get_time())
 
     # load expert trajectories
     expert_revisions = PYTHIA_REVISIONS
@@ -119,7 +118,6 @@ def main(args):
 
         start_epoch = np.random.randint(0, len(buffer) - args.expert_epochs)
         starting_params = buffer[start_epoch]
-        print("start_epoch", start_epoch, "len(buffer)", len(buffer))
 
         target_params = buffer[start_epoch+args.expert_epochs]
         target_params = torch.cat([p.data.to(args.device).reshape(-1) for n, p in target_params.items()], 0)
@@ -131,6 +129,7 @@ def main(args):
         param_loss_list = []
         param_dist_list = []
         indices_chunks = []
+        mse_losses = []
 
         for step in tqdm.trange(args.syn_steps, desc="Synthetic Steps", leave=False):
             if not indices_chunks:
@@ -151,88 +150,88 @@ def main(args):
                 word_embeddings = student_net.module.get_input_embeddings().to(args.device)
 
             outputs_softmax = F.softmax(output.logits[:, :-1], dim=-1)
-            mimiced_embeddings = torch.einsum("bsv,vd->bsd", outputs_softmax, word_embeddings.weight)
-            mimiced_embeddings = mimiced_embeddings.view(-1, hidden_size)
+            outputs_softmax = outputs_softmax.contiguous().view(-1, word_embeddings.weight.shape[0])
 
             x_inputs = x[:, 1:, :].contiguous().view(-1, hidden_size)
-            assert x_inputs.shape == mimiced_embeddings.shape, f"invalid shape: {x_inputs.shape} != {mimiced_embeddings.shape}"
-            mse_loss = torch.nn.functional.mse_loss(mimiced_embeddings, x_inputs, reduction="sum") / sequence_length
-            print("mse_loss:", mse_loss)
+            x_inputs_logits = x_inputs @ word_embeddings.weight.T
+            inputs_softmax = F.softmax(x_inputs_logits, dim=-1)
+
+            
+            assert outputs_softmax.shape == inputs_softmax.shape, f"invalid shape: {outputs_softmax.shape} != {inputs_softmax.shape}"
+            # mse_loss = torch.nn.functional.mse_loss(mimiced_embeddings, x_inputs, reduction="sum") / sequence_length
+            mse_loss = torch.nn.functional.cross_entropy(outputs_softmax, inputs_softmax, reduction="sum") / sequence_length
+            mse_losses.append(mse_loss.detach().item())
 
             # exit on nan
             if torch.isnan(mse_loss):
                 print("nan detected - stopping!")
-                break
+                exit()
 
             grad = torch.autograd.grad(mse_loss, student_params[-1], create_graph=True)[0]
             student_params.append(student_params[-1] - syn_lr * grad)
 
 
-        param_loss = torch.tensor(0.0).to(args.device)
-        param_dist = torch.tensor(0.0).to(args.device)
+        param_loss = torch.nn.functional.mse_loss(student_params[-1], target_params, reduction="sum")
+        param_dist = torch.nn.functional.mse_loss(starting_params, target_params, reduction="sum")
 
-        param_loss += torch.nn.functional.mse_loss(student_params[-1], target_params, reduction="sum")
-        param_dist += torch.nn.functional.mse_loss(starting_params, target_params, reduction="sum")
         param_loss_list.append(param_loss)
         param_dist_list.append(param_dist)
 
-
         param_loss /= num_params
         param_dist /= num_params
-
         param_loss /= param_dist
-
-        grand_loss = param_loss
 
         optimizer_token_embeddings.zero_grad()
         optimizer_lr.zero_grad()
     
         # https://github.com/pytorch/pytorch/issues/116350
-        grand_loss.backward()
+        param_loss.backward()
 
         optimizer_token_embeddings.step()
         optimizer_lr.step()
 
-        wandb.log({"Grand_Loss": grand_loss.detach().cpu(),
-                   "Start_Epoch": start_epoch})
+        mse_loss_avg = sum(mse_losses) / len(mse_losses)
+        wandb.log(
+            {
+                "param_loss": param_loss.detach().cpu(),
+                "param_loss_minus_one": (param_loss - 1).detach().cpu(),
+                "param_dist": param_dist.detach().cpu(),
+                "start_epoch": start_epoch,
+                "mse_loss": mse_loss_avg,
+            }
+        )
 
         for _ in student_params:
             del _
 
         if it%10 == 0:
-            print('%s iter = %04d, loss = %.4f' % (get_time(), it, grand_loss.item()))
+            print('%s iter = %04d, loss = %.8f, oneofloss = %.8f, mse_loss = %.8f' % (get_time(), it, param_loss.item(), (param_loss - 1).item(), mse_loss_avg))
 
     wandb.finish()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parameter Processing')
-    parser.add_argument('--res', type=int, default=128, help='resolution for imagenet')
     parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode, check utils.py for more info')
 
     parser.add_argument("--dataset_size", "--ds", type=int, default=1000, help="size of distilled dataset")
-
     parser.add_argument('--num_eval', type=int, default=5, help='how many networks to evaluate on')
-
     parser.add_argument('--eval_it', type=int, default=100, help='how often to evaluate')
-
     parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data')
+
     parser.add_argument('--Iteration', type=int, default=5000, help='how many distillation steps to perform')
 
     parser.add_argument('--lr_img', type=float, default=1000, help='learning rate for updating synthetic images')
     parser.add_argument('--lr_lr', type=float, default=1e-05, help='learning rate for updating... learning rate')
     parser.add_argument('--lr_teacher', type=float, default=0.01, help='initialization for synthetic learning rate')
 
-    parser.add_argument('--lr_init', type=float, default=0.01, help='how to init lr (alpha)')
-
     parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
     parser.add_argument('--batch_syn', type=int, default=None, help='should only use this if you run out of VRAM')
     parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
     parser.add_argument('--model_name', type=str, default='EleutherAI/pythia-70m', help='model name')
 
-    parser.add_argument('--expert_epochs', type=int, default=3, help='how many expert epochs the target params are')
+    parser.add_argument('--expert_epochs', type=int, default=1, help='how many expert epochs the target params are')
     parser.add_argument('--syn_steps', type=int, default=20, help='how many steps to take on synthetic data')
-    parser.add_argument('--max_start_epoch', type=int, default=25, help='max epoch we can start at')
     parser.add_argument('--max_experts', type=int, default=16, help='number of experts to read per file (leave as None unless doing ablations)')
     parser.add_argument('--force_save', action='store_true', help='this will save images for 50ipc')
     parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
