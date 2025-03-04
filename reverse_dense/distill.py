047b2ef9e6c46439ca1ab8314eddde427f3b02a0
Jack Morris <jxmorris12@gmail.com>
2025-03-03 22:00:01 -0500

    add initialization methods

--- reverse_dense/distill.py
+++ reverse_dense/distill.py
@@ -25,12 +25,8 @@ sequence_length = 32
 text_column_name = "text"        
 label_column_name = "label"
 
-num_experts = 5
-num_steps_per_expert = 200
-
-num_experts = 2
-num_steps_per_expert = 20
-
+num_experts = 20
+num_steps_per_expert = 100
 num_expert_datapoints = 128
 expert_lr = 1e-4
 
@@ -38,15 +34,16 @@ expert_lr = 1e-4
 def state_dict_to_tensor(ordered_dict: dict[str, torch.Tensor]) -> torch.Tensor:
     return torch.cat([p.data.to(device).reshape(-1) for n, p in ordered_dict.items()], 0)
 
-def get_token_embeddings_syn(ds: datasets.Dataset) -> tuple[torch.Tensor, torch.Tensor]:
+
+def get_token_embeddings_from_dataset(ds: datasets.Dataset) -> tuple[torch.Tensor, torch.Tensor]:
     """ initialize the synthetic data """
     student_net = get_model("gpt2")
     student_net.train()
     student_token_embeddings = student_net.get_input_embeddings().to(device)
     # TODO: Consider other initialization methods
-    def simple_soft_one_hot(x, num_classes, temperature=1.0):
-        one_hot = F.one_hot(x, num_classes).float()
-        return F.softmax(one_hot / temperature, dim=-1)
+    # def simple_soft_one_hot(x, num_classes, temperature=1.0):
+    #     one_hot = F.one_hot(x, num_classes).float()
+    #     return F.softmax(one_hot / temperature, dim=-1)
     
     hidden_size = student_token_embeddings.weight.shape[1]
 
@@ -70,7 +67,30 @@ def get_token_embeddings_syn(ds: datasets.Dataset) -> tuple[torch.Tensor, torch.
     token_labels_syn = torch.randint(low=0, high=num_classes, size=[args.dataset_size], device=device)
     token_labels_syn = CLASS_MAP[token_labels_syn]
 
-    token_embeddings_syn = torch.randn_like(token_embeddings_syn, device=device) # Actually just do random init.
+    return (token_embeddings_syn, token_labels_syn)
+
+def get_token_embeddings_random() -> tuple[torch.Tensor, torch.Tensor]:
+    """ initialize the synthetic data """
+    student_net = get_model("gpt2")
+    student_net.train()
+    student_token_embeddings = student_net.get_input_embeddings().to(device)
+    tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
+
+    def simple_soft_one_hot(x, num_classes, temperature=1.0):
+        one_hot = F.one_hot(x, num_classes).float()
+        return F.softmax(one_hot / temperature, dim=-1)
+    
+    rand_token_idxs = torch.randint(0, student_token_embeddings.num_embeddings, (args.dataset_size, sequence_length-1), device=device)
+    rand_token_one_hots = simple_soft_one_hot(rand_token_idxs, student_token_embeddings.num_embeddings, temperature=0.1)
+    token_embeddings_syn = rand_token_one_hots @ student_token_embeddings.weight
+    
+    hidden_size = student_token_embeddings.weight.shape[1]
+    assert token_embeddings_syn.shape == (args.dataset_size, sequence_length - 1, hidden_size), f"invalid shape: {token_embeddings_syn.shape}, need {(args.dataset_size, sequence_length - 1, hidden_size)}"
+
+    num_classes = 4
+    CLASS_MAP = torch.tensor([352,  362,  657,  513], device=device) # for AG_News... tmp
+    token_labels_syn = torch.randint(low=0, high=num_classes, size=[args.dataset_size], device=device)
+    token_labels_syn = CLASS_MAP[token_labels_syn]
 
     return (token_embeddings_syn, token_labels_syn)
 
@@ -147,10 +167,6 @@ def load_expert_paths(ds: datasets.Dataset):
             step += 1
         expert_paths.append(_get_state_dict(student_net))
 
-    # # random.shuffle(expert_revisions)
-    # if args.max_experts is not None:
-    #     expert_start_epoch = random.randint(0, len(expert_paths) - args.max_experts)
-    #     expert_paths = expert_paths[expert_start_epoch:expert_start_epoch + args.max_experts]
     return expert_paths
 
 
@@ -180,7 +196,13 @@ def main(args):
     print('Hyperparameters: \n', dict(vars(args)))
 
     ds = datasets.load_dataset("fancyzhx/ag_news", split="train")
-    X, Y = get_token_embeddings_syn(ds=ds)
+
+    if args.token_init == "random":
+        X, Y = get_token_embeddings_random()
+    elif args.token_init == "dataset":
+        X, Y = get_token_embeddings_from_dataset(ds=ds)
+    else:
+        raise NotImplementedError(f"Token init {args.token_init} not implemented")
     X = X.detach().to(device).requires_grad_(True)
 
     # train model for a couple steps
@@ -207,7 +229,8 @@ def main(args):
     # load expert trajectories
     initial_student_net = get_model("gpt2")
     buffer = load_expert_paths(ds=ds)
-    for it in tqdm.trange(0, args.max_iterations+1, desc="iterations"):
+    pbar = tqdm.trange(0, args.max_iterations+1, desc="iterations")
+    for it in pbar:
         num_params = sum([np.prod(p.size()) for p in (student_net.parameters())])
 
         start_epoch = np.random.randint(0, len(buffer) - args.expert_epochs)
@@ -265,7 +288,6 @@ def main(args):
 
         aux_loss = (ρ / 2) * (X - Z - Λ / ρ).norm(p=2, dim=2).mean()
 
-        print("aux_loss", aux_loss, "param_loss", param_loss)
         if (it >= args.pretrain_iterations_x):
             (aux_loss + param_loss).backward()
         else:
@@ -282,10 +304,9 @@ def main(args):
                 #                       TODO: Use a language model for this, optionally.
                 embeddings = initial_student_net.get_input_embeddings()
                 embeddings = embeddings.to(device)
-                embeddings_norm = (embeddings.weight / embeddings.weight.norm(p=2, dim=1, keepdim=True)).to(device)
                 X_proj = X + Λ / ρ
-                X_norm = X_proj / X_proj.norm(p=2, dim=2, keepdim=True)
-                Z_tokens = (X_norm @ embeddings_norm.T).argmax(dim=-1)
+                Z_distances = torch.cdist(X_proj, embeddings.weight.to(device))
+                Z_tokens = Z_distances.argmin(dim=2)
                 Z = embeddings(Z_tokens)
                 # 
                 # Log Z
@@ -321,9 +342,16 @@ def main(args):
             },
             step=it
         )
-        print("%s step = %04d, loss = %.8f, oneoffloss = %.8f, ce_loss = %.8f" % (get_time(), it, param_loss.item(), (param_loss - 1).item(), ce_loss_avg))
-
-
+        # print("aux_loss", aux_loss, "param_loss", param_loss)
+        # print("%s step = %04d, loss = %.8f, oneoffloss = %.8f, ce_loss = %.8f" % (get_time(), it, param_loss.item(), (param_loss - 1).item(), ce_loss_avg))
+        pbar.set_postfix(
+            aux_loss=f"{aux_loss.item():.3f}",
+            param_loss=f"{param_loss.item():.3f}",
+            ce_loss=f"{ce_loss_avg:.3f}",
+            param_dist=f"{param_dist.item():.3f}",
+            Z_dist=f"{Z_dist.item():.3f}",
+            lr=f"{syn_lr.item():.3f}",
+        )
         for _ in student_params:
             del _
     wandb.finish()
@@ -331,9 +359,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode, check utils.py for more info')
-
     parser.add_argument("--dataset_size", "--ds", type=int, default=1000, help="size of distilled dataset")
+    parser.add_argument('--batch_syn', type=int, default=None, help='should only use this if you run out of VRAM')
+
     parser.add_argument('--max_iterations', type=int, default=5000, help='how many distillation steps to perform')
     parser.add_argument('--max_iterations_x', type=int, default=40, help='how many gradient steps per X update')
     parser.add_argument('--pretrain_iterations_x', type=int, default=100, help='how many gradient steps in initial X training phase')
@@ -341,15 +369,11 @@ if __name__ == '__main__':
     parser.add_argument('--lr_tokens', type=float, default=0.001, help='learning rate for updating synthetic tokens')
     parser.add_argument('--lr_lr', type=float, default=1e-05, help='learning rate for updating... learning rate')
     parser.add_argument('--lr_teacher', type=float, default=0.01, help='initialization for synthetic learning rate')
-
-    parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
-    parser.add_argument('--batch_syn', type=int, default=None, help='should only use this if you run out of VRAM')
-    parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
-
     parser.add_argument('--expert_epochs', type=int, default=1, help='how many expert epochs the target params are')
     parser.add_argument('--syn_steps', type=int, default=8, help='how many steps to take on synthetic data')
     parser.add_argument('--max_experts', type=int, default=32, help='number of experts to read per file (leave as None unless doing ablations)')
     parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument("--token_init", type=str, default="random", help="how to initialize the synthetic tokens", choices=["random", "dataset"])
 
     args = parser.parse_args()
 
