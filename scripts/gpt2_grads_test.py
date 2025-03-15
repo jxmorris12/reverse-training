c56b77197c27668439a811abf9ec9c07a0a2d385
jxmorris12 <jxmorris12@gmail.com>
2025-03-15 17:04:53 +0000

    updating gpt2_grads_test [2/2]

--- scripts/gpt2_grads_test.py
+++ scripts/gpt2_grads_test.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Sequence, Union
+from typing import Dict, Sequence, Union, Optional
 from dataclasses import dataclass
 
+import collections
 import copy
+import random
+
 import datasets
 import torch
 import transformers
@@ -12,6 +15,22 @@ from enum import Enum
 from torch.utils.data import Dataset
 
 # from utils import train_expert_model
+
+def limit_layers(model: transformers.PreTrainedModel, n_layers: int) -> transformers.PreTrainedModel:
+    if hasattr(model, 'transformer'):
+        if hasattr(model.transformer, 'h'):
+            model.transformer.h = model.transformer.h[:n_layers]
+        else:
+            model.transformer.layer = model.transformer.layer[:n_layers]
+    elif hasattr(model, 'encoder'):
+        if hasattr(model.encoder, 'layers'):
+            model.encoder.layers = model.encoder.layers[:n_layers]
+        else:
+            model.encoder.layer = model.encoder.layer[:n_layers]
+    else:
+        raise RuntimeError(f"unknown how to limit layers of model {type(model)}")
+    return model
+
 
 class ProjectionType(str, Enum):
     rademacher = "rademacher"
@@ -152,6 +171,10 @@ class CudaProjector(AbstractProjector):
     ) -> torch.Tensor:
         if isinstance(grads, dict):
             grads = vectorize(grads, device=self.device)
+        
+        # Optionally add a batch dimension
+        if grads.ndim == 1:
+            grads = grads[None]
 
         batch_size = grads.shape[0]
 
@@ -195,138 +218,105 @@ class CudaProjector(AbstractProjector):
         """A no-op method."""
         pass
 
-
-IGNORE_INDEX = -100
-DEFAULT_PAD_TOKEN = "[PAD]"
-DEFAULT_EOS_TOKEN = "</s>"
-DEFAULT_BOS_TOKEN = "<s>"
-DEFAULT_UNK_TOKEN = "<unk>"
-PROMPT_DICT = {
-    "prompt_input": (
-        "Below is an instruction that describes a task, paired with an input that provides further context. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
-    ),
-    "prompt_no_input": (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Response:"
-    ),
-}
-
-def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer) -> Dict:
-    """Tokenize a list of strings."""
-    tokenized_list = [
-        tokenizer(
-            text,
-            return_tensors="pt",
-            padding="longest",
-            max_length=tokenizer.model_max_length,
-            truncation=True,
-        )
-        for text in strings
-    ]
-    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
-    input_ids_lens = labels_lens = [
-        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
-    ]
-    return dict(
-        input_ids=input_ids,
-        labels=labels,
-        input_ids_lens=input_ids_lens,
-        labels_lens=labels_lens,
-    )
-
-
-def preprocess(
-    sources: Sequence[str],
-    targets: Sequence[str],
-    tokenizer: transformers.PreTrainedTokenizer,
-) -> Dict:
-    """Preprocess the data by tokenizing."""
-    examples = [s + t for s, t in zip(sources, targets)]
-    examples_tokenized, sources_tokenized = [_tokenize_fn(strings, tokenizer) for strings in (examples, sources)]
-    input_ids = examples_tokenized["input_ids"]
-    labels = copy.deepcopy(input_ids)
-    for label, source_len in zip(labels, sources_tokenized["input_ids_lens"]):
-        label[:source_len] = IGNORE_INDEX
-    return dict(input_ids=input_ids, labels=labels)
-
-class SupervisedDataset(Dataset):
-    """Dataset for supervised fine-tuning."""
-
-    def __init__(self, list_data_dict: datasets.Dataset, tokenizer: transformers.PreTrainedTokenizer):
-        super(SupervisedDataset, self).__init__()
-        prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
-        sources = [
-            prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
-            for example in list_data_dict
-        ]
-        targets = [f"{example['output']}{tokenizer.eos_token}" for example in list_data_dict]
-        data_dict = preprocess(sources, targets, tokenizer)
-
-        self.input_ids = data_dict["input_ids"]
-        self.labels = data_dict["labels"]
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
-        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
-
-
-@dataclass
-class DataCollatorForSupervisedDataset(object):
-    """Collate examples for supervised fine-tuning."""
-
-    tokenizer: transformers.PreTrainedTokenizer
-
-    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
-        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
-        input_ids = torch.nn.utils.rnn.pad_sequence(
-            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
-        )
-        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
-        return dict(
-            input_ids=input_ids,
-            labels=labels,
-            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
-        )
-
-
-def smart_tokenizer_and_embedding_resize(
-    special_tokens_dict: Dict,
-    tokenizer: transformers.PreTrainedTokenizer,
-    model: transformers.PreTrainedModel,
-):
-    """Resize tokenizer and embedding.
-
-    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
-    """
-    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
-    model.resize_token_embeddings(len(tokenizer))
-
-    if num_new_tokens > 0:
-        input_embeddings = model.get_input_embeddings().weight.data
-        output_embeddings = model.get_output_embeddings().weight.data
-
-        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
-        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
-
-        input_embeddings[-num_new_tokens:] = input_embeddings_avg
-        output_embeddings[-num_new_tokens:] = output_embeddings_avg
-
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def get_model(model_path: str) -> dict[str, torch.Tensor]:
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_path, 
+        attn_implementation="eager"
+    )
+    return limit_layers(model, 6)
+
+def _get_state_dict(model: torch.nn.Module) -> dict[str, torch.Tensor]:
+    state_dict = model.state_dict()
+    
+    # hack to get around gpt weight-tying:
+    if torch.isclose(state_dict["transformer.wte.weight"], state_dict["lm_head.weight"]).all():
+        del state_dict["lm_head.weight"]
+    
+    return { k: v.detach().clone() for k,v in state_dict.items() }
 
 
-def get_grads(base_model, dataset, data_collator, projector) -> torch.Tensor:
+def get_model_state_dict(model_path: str) -> dict[str, torch.Tensor]:
+    return _get_state_dict(get_model(model_path))
+
+def train_expert_model(
+        num_experts: int, 
+        num_steps_per_expert: int, 
+        num_expert_datapoints: int, 
+        expert_lr: float, 
+        sequence_length: int,
+        train_ds: datasets.Dataset, 
+        tokenizer: transformers.AutoTokenizer,
+        text_column_name: str,
+    ) -> tuple[list[dict[str, torch.Tensor]], torch.Tensor]:
+    student_net = get_model("gpt2").to(device)
+
+    # optim = torch.optim.Adam(student_net.parameters(), lr=expert_lr)
+    optim = torch.optim.SGD(student_net.parameters(), lr=expert_lr)
+
+    expert_state_dicts = [_get_state_dict(student_net)]
+    step = 0
+    pbar = tqdm.tqdm(total=num_experts * num_steps_per_expert, colour="CYAN")
+    all_token_counts = torch.zeros([tokenizer.vocab_size], device=device)
+
+    # training loop
+    for _i in range(num_experts):
+        for _j in range(num_steps_per_expert):
+            batch_idxs = random.sample(
+                range(len(train_ds)), 
+                k=num_expert_datapoints
+            )
+            examples = train_ds.select(batch_idxs)[text_column_name]
+            tokens = tokenizer(
+                examples,
+                return_tensors="pt", 
+                padding="max_length", 
+                truncation=True, 
+                max_length=sequence_length, 
+            ).to(device)
+            outputs = student_net(
+                input_ids=tokens.input_ids,
+                attention_mask=tokens.attention_mask,
+            )
+            # use bincount to track token counts
+            all_token_counts += torch.bincount(tokens.input_ids.flatten(), minlength=tokenizer.vocab_size)
+            
+            labels = tokens.input_ids[:, 1:].detach().clone() 
+            logits = outputs.logits[:, :-1, :]
+            loss = torch.nn.functional.cross_entropy(
+                logits.reshape(labels.numel(), -1), 
+                labels.reshape(labels.numel(),),
+                ignore_index=-100,
+                reduction="mean"
+            )
+            pbar.set_description(f"Expert training step {step+1} | Loss = {loss:.3f}")
+            pbar.update(1)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+            student_net.zero_grad()
+            step += 1
+        expert_state_dicts.append(_get_state_dict(student_net))
+    
+    pbar.close()
+
+    return expert_state_dicts, all_token_counts
+
+
+def get_grads(base_model, dataset, tokenizer, projector, sequence_length: int) -> torch.Tensor:
     all_grads = []
     pbar = tqdm.trange(0, len(dataset), 1)
     for i in pbar:
         batch = dataset[i]
-        inputs = data_collator([batch])
+        inputs = tokenizer(
+            batch["text"],
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=sequence_length,
+        )
         inputs = { k: v.to(device) for k, v in inputs.items() }
+        inputs["labels"] = inputs["input_ids"].detach().clone()
         outputs = base_model(**inputs)
         outputs.loss.backward()
         grad_flattened = torch.cat([
@@ -339,52 +329,45 @@ def get_grads(base_model, dataset, data_collator, projector) -> torch.Tensor:
         base_model.zero_grad()
     return torch.stack(all_grads)
 
-def main():
-    base_model = transformers.AutoModelForCausalLM.from_pretrained("gpt2")
-    other_model = transformers.AutoModelForCausalLM.from_pretrained("vicgalle/gpt2-alpaca")
 
+def main():
+    base_model = get_model("gpt2")
+    base_model.to(device)
+
+    train_dataset = datasets.load_dataset("tatsu-lab/alpaca")
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         "gpt2",
         model_max_length=256,
         padding_side="right",
-        use_fast=False,
+        truncation_side="right",
     )
-    special_tokens_dict = dict()
-    if tokenizer.pad_token is None:
-        special_tokens_dict["pad_token"] = DEFAULT_PAD_TOKEN
-    if tokenizer.eos_token is None:
-        special_tokens_dict["eos_token"] = DEFAULT_EOS_TOKEN
-    if tokenizer.bos_token is None:
-        special_tokens_dict["bos_token"] = DEFAULT_BOS_TOKEN
-    if tokenizer.unk_token is None:
-        special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
-    smart_tokenizer_and_embedding_resize(
-        special_tokens_dict=special_tokens_dict,
-        tokenizer=tokenizer,
-        model=other_model,
-    )
-    smart_tokenizer_and_embedding_resize(
-        special_tokens_dict=special_tokens_dict,
-        tokenizer=tokenizer,
-        model=base_model,
-    )
-    
+    tokenizer.pad_token = tokenizer.eos_token
 
-    # average params
-    for (n1, p1), (n2, p2) in zip(base_model.named_parameters(), other_model.named_parameters()):
-        p1.data = (p1.data + p2.data) / 2
-    
+    def make_text(ex: dict[str, str]) -> dict[str, str]:
+        ex["text"] = ex["instruction"] + "\n" + ex["output"]
+        return ex
+    train_dataset = train_dataset.map(make_text)
+
+    # train_dataset = train_dataset.select(range(4_096))
+    train_dataset["train"] = train_dataset["train"].select(range(64))
+    expert_state_dicts, _ = train_expert_model(
+        num_experts=1,
+        num_steps_per_expert=100,
+        num_expert_datapoints=32,
+        expert_lr=1e-2, 
+        sequence_length=32,
+        train_ds=train_dataset["train"],
+        tokenizer=tokenizer,
+        text_column_name="text",
+    )
+    expert_model_params = expert_state_dicts[-1]
+    initial_expert_model_params = torch.cat([p.flatten() for p in expert_model_params.values()]).double().to(device).requires_grad_(False)
 
     base_params = torch.cat([p.flatten() for p in base_model.parameters()]).double().to(device)
-    other_params = torch.cat([p.flatten() for p in other_model.parameters()]).double().to(device)
-    param_diff = base_params - other_params
     number_of_params = len(base_params)
 
-    train_dataset = datasets.load_dataset("tatsu-lab/alpaca")["train"]
-    # train_dataset = train_dataset.select(range(4_096))
-    train_dataset = train_dataset.select(range(64))
-
-    dataset = SupervisedDataset(train_dataset, tokenizer)
+    initial_mse = (base_params[None] - initial_expert_model_params[None]).double().pow(2).mean().item()
+    print(f"Initial parameter MSE: {initial_mse:.8f}")
 
     projection_dim = 1024
     projector = CudaProjector(
@@ -396,52 +379,76 @@ def main():
         max_batch_size=32
     )
 
-    # get all grads
-    base_model.to(device)
-
-    # initial filtering
-    data_collator = DataCollatorForSupervisedDataset(tokenizer)
-    all_grads = get_grads(base_model, dataset, data_collator, projector)
-    
-    # Compute similarities with projected param_diff
-    projected_param_diff = projector.project(param_diff.unsqueeze(0), model_id=0).squeeze(0)
-    all_sims = torch.nn.functional.cosine_similarity(all_grads, projected_param_diff.unsqueeze(0), dim=1)
-    
-    top_k = 1000
-    top_k_indices = torch.argsort(all_sims, descending=True)[:top_k]
-
     # train + filter
-    train_dataset = train_dataset.select(top_k_indices)
-    filtered_train_dataset = train_dataset
-    dataset = SupervisedDataset(filtered_train_dataset, tokenizer)
+    student_lr = 1e-2
+    optimizer = torch.optim.SGD(base_model.parameters(), lr=student_lr)
     batch_size = 8
-    steps_before_reranking = 10
-    optimizer = torch.optim.AdamW(base_model.parameters(), lr=1e-5)
     j = 0
-    num_steps = min(1000, len(dataset)) // batch_size
+    # num_steps = min(1000, len(train_dataset)) // batch_size
+    num_steps = 10
     pbar = tqdm.trange(0, num_steps)
+
     for _ in pbar:
-        batch = [dataset[j] for j in range(j * batch_size, (j + 1) * batch_size)]
-        inputs = data_collator(batch)
+        # get all grads
+        grads = get_grads(base_model, train_dataset["train"], tokenizer, projector, sequence_length=32)
+        assert grads.shape == (len(train_dataset["train"]), projection_dim)
+
+        # project params
+        base_params = torch.cat([p.flatten() for p in base_model.parameters()]).double().to(device)
+        base_params = projector.project(base_params, model_id=0)
+        expert_model_params = projector.project(initial_expert_model_params, model_id=0)
+
+        projected_mse = (base_params - expert_model_params).double().pow(2).mean().item()
+        print(f"Projected MSE: {projected_mse:.8f}")
+
+        # greedily fill batch
+        batch = []
+        batch_grads_sum = torch.zeros_like(grads[0])
+        while len(batch) < batch_size:
+            # minimize |\sum_{i} batch_i  + x - grads_i|^2
+            # sims = torch.nn.functional.cosine_similarity(
+            #     batch_grads_sum[None] + grads + base_params,
+            #     expert_model_params,
+            #     dim=1
+            # )
+            # use MSE
+            new_base_params = base_params + grads * student_lr
+            sims = (new_base_params - expert_model_params).double().pow(2).mean(dim=1)
+            # check for nans
+            if torch.isnan(sims).any():
+                print("NaNs in sims")
+                breakpoint()
+                break
+            # filter out already selected grad
+            batch_idxs = torch.tensor(batch, dtype=torch.int64)
+            sims[batch_idxs] = float("inf")
+            best_idx = torch.argmin(sims)
+            batch.append(best_idx)
+            batch_grads_sum += grads[best_idx]
+            grads[best_idx] = torch.zeros_like(grads[best_idx])
+            print(f"Best sim: {sims[best_idx].item():.3f} | Batch size: {len(batch)} | Best idx: {best_idx}")
+
+        # take step on batch
+        inputs = tokenizer(
+            train_dataset["train"].select(batch)["text"],
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=32,
+        )
         inputs = { k: v.to(device) for k, v in inputs.items() }
+        inputs["labels"] = inputs["input_ids"].detach().clone()
         outputs = base_model(**inputs)
         loss = outputs.loss
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        # print(f"loss: {loss.item()}")
         pbar.set_description(f"loss: {loss.item():.3f}")
+        print(f"loss: {loss.item():.3f}")
         j += 1
-        if (j > 0) and (j % steps_before_reranking == 0):
-            all_grads = get_grads(base_model, dataset, data_collator, projector)
-            projected_param_diff = projector.project(param_diff.unsqueeze(0), model_id=0).squeeze(0)
-            all_sims = torch.nn.functional.cosine_similarity(all_grads, projected_param_diff.unsqueeze(0), dim=1)
-            top_indices = torch.argsort(all_sims, descending=True)
-            filtered_train_dataset = train_dataset.select(top_indices)
-            dataset = SupervisedDataset(filtered_train_dataset, tokenizer)
-            j = 0
-            breakpoint()
-
-
+    
+    final_mse = (base_params[None] - expert_model_params[None]).double().pow(2).mean().item()
+    print(f"Final parameter MSE: {final_mse:.3f}")
+    
 if __name__ == "__main__":
     main()
